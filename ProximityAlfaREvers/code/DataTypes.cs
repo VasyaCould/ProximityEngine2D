4d1fd7c6278445b79beff7c91a7d4ee154af0495
VasyaCould <Vasyazhavoronkov@gmail.com>
2025-05-25 23:17:50 +0500

    Убран дублированный класс Vector2Int

--- ProximityAlfaREvers/code/DataTypes.cs
+++ ProximityAlfaREvers/code/DataTypes.cs
@@ -33,21 +33,6 @@ namespace engine
             this.x = 0;
         }
     }
-    public class Vector2Int
-    {
-        public int x;
-        public int y;
-        public Vector2Int(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-        public Vector2Int()
-        {
-            this.y = 0;
-            this.x = 0;
-        }
-    }
     public class GameObject
     {
         public string programName, imgDirectory;
